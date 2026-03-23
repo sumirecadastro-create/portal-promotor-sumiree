a4578c23962f0ae1a8f9acb07413e733ba2e2661
Skip <skip@usecurling.com>
2026-03-23 18:25:25 +0000

    Auto-commit before sync

--- src/lib/pocketbase/client.ts
+++ src/lib/pocketbase/client.ts
@@ -1,9 +1,6 @@
 import PocketBase from 'pocketbase'
 
-// URL CORRETA do backend (PocketBase)
-const BACKEND_URL = 'https://backend-portal-promotor-sumire-f82ca.goskip.app'
-
-const pb = new PocketBase(BACKEND_URL)
+const pb = new PocketBase(import.meta.env.VITE_POCKETBASE_URL)
 pb.autoCancellation(false)
 
 export default pb
