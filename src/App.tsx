ea4d3a188d500403b1475ca825ad1a3e0c54213e
sumirecadastro-create <sumirecadastro@gmail.com>
2026-04-28 09:04:43 -0300

    Fix import statements for AuthProvider and ProtectedRoute

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,8 @@ import Relatorios from './pages/Relatorios'
 import Campanhas from './pages/Campanhas' 
 import NotFound from './pages/NotFound'
 import Login from './pages/Login'
-import { AuthProvider } from './hooks/use-auth'
-import { ProtectedRoute } from './components/ProtectedRoute'
+import  AuthProvider  from './hooks/use-auth'
+import  ProtectedRoute  from './components/ProtectedRoute'
 
 const App = () => (
   <BrowserRouter>
